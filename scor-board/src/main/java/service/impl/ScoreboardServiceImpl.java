ebda5b05d77f3d9fa8833654bdd1637655368552
ismael1987 <ismael-1987@hotmail.com>
2026-03-29 13:08:57 +0200

    implement the UpdateScore method

--- scor-board/src/main/java/service/impl/ScoreboardServiceImpl.java
+++ scor-board/src/main/java/service/impl/ScoreboardServiceImpl.java
@@ -12,6 +12,8 @@ import java.util.stream.Collectors;
 public class ScoreboardServiceImpl implements IScoreboardService {
 
     private final List<Match> matches = new ArrayList<>();
+    private static final String MATCH_NOT_FOUND = "Match not found on scoreboard";
+    private static final String MATCH_NOT_ABS = "Scores cannot be negative";
 
     @Override
     public Match startMatch(Team home, Team away) {
@@ -31,6 +33,17 @@ public class ScoreboardServiceImpl implements IScoreboardService {
 
     @Override
     public void updateScore(Match match, long homeScore, long awayScore) throws IllegalArgumentException {
+        validateMatchExists(match);
+        if (homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException(MATCH_NOT_ABS);
+        }
+        match.setHomeScore(homeScore);
+        match.setAwayScore(awayScore);
+    }
 
+    private void validateMatchExists(Match match) {
+        if (!matches.contains(match)) {
+            throw new IllegalArgumentException(MATCH_NOT_FOUND);
+        }
     }
 }
